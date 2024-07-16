659fb04bd677e0e176d367e2d660882781b9231c
Chinaza <98832473+Chiboy06@users.noreply.github.com>
2024-07-16 09:24:47 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3001",
+        target: "https://meeting-auth-api-2.onrender.com",
         changeOrigin: true,
       }
     }
