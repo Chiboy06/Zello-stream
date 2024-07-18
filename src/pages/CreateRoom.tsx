fb4f03867f0ff91386ef89a30e8317873194ca6c
Chinaza <elechukwujosh3@gmail.com>
2024-07-18 03:52:52 +0200

    video layout

--- src/pages/CreateRoom.tsx
+++ src/pages/CreateRoom.tsx
@@ -76,8 +76,8 @@ const CreateRoom = ({ reject }: CustomCancelCallBtn) => {
   
 
   return (
-    <div className='h-svh bg-slate-900 scroll-m-0'>
-      <div className='flex flex-col md:flex-row'>
+    <div className='h-svh bg-slate-900 overflow-y-hidden'>
+      <div className='flex flex-col md:flex-row h-[100%]'>
         <StreamTheme style={{ position: 'relative' }}>    
           <StreamCall call={call}>
             <SpeakerLayout />
